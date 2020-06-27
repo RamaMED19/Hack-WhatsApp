ddadaec83c1871f9bff74bddc4a58bca691f9c09
RamaMED19 <67484406+RamaMED19@users.noreply.github.com>
2020-06-27 12:55:58 +0700

    Aku cinta nasi padang

---
+++ GasGayn.sh
@@ -0,0 +1,152 @@
+#!/usr/bin/bash
+#tools sederhana
+#prank hack satelit
+#by LxG_9 Gans
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+echo " "
+echo  $blue "<|======================================|>"
+echo  $red    "|       WELCOME TO MY TOOLS          |"
+echo  $red    "|                                    |"
+echo  $red    "| Author        : Mr.XaHmd022        |"
+echo  $red    "| THX           : M.ZulFdli(Jibon)   |"
+echo  $red    "| iNSTGRAM      : @ahmed_78988       |"
+echo  $red    "| YOUTUBE       : Rama Ahmed         |"
+echo  $blue "<|======================================|>"
+sleep 1
+echo " "
+echo $green "============== $red MENU $green ================="
+echo $blue "===================================="
+echo $blue "|| $green 1. Hack WA.     [ON]            $blue ||"
+echo $blue "|| $green 2. HACK IG.     [OF]            $blue ||"
+echo $blue "|| $green 3. HACK FB.     [OF]            $blue ||"
+echo $blue "|| $green 4. HACK YAHOO.  [OF]            $blue ||"
+echo $blue "|| $green 0. Exit The Program             $blue ||"
+echo $blue "===================================="
+echo $blue"╭──[Masukkan Pilihan Anda]>"
+read -p"╰───────root@RmaAhMed >" Gans;
+if [ $Gans = 1 ] || [ $Gans = 01 ]
+then
+echo $blue"╭──NO Telp target"
+read -p"╰───────root@RmaAhMed >" arah;
+sleep 1
+echo $red "Scaning"
+sleep 1
+echo $blue "10%"
+sleep 1
+echo $green "20%"
+sleep 1
+echo $cyan "30%"
+sleep 1
+echo $purple "40%"
+sleep 1
+echo $blue "50%"
+sleep 1
+echo $red "60%"
+sleep 1
+echo $green "70%"
+sleep 1
+echo $cyan "80%"
+sleep 1
+echo $purple "90%"
+sleep 1
+echo $blue "100%"
+sleep 1
+echo $green "Prosessing Done"
+sleep 1
+echo  $blue "DOSA TANGGUNG SENDIRI SLURR.. "
+fi
+
+echo $red "PROSES CRACK KODE"
+sleep 2
+echo $red "753-736"
+sleep 1
+echo $red "541-978"
+sleep 1
+echo $red "126-743"
+sleep 1
+echo $red "168-854"
+sleep 1
+echo $red "976-095"
+sleep 1
+echo $red "579-416"
+sleep 1
+echo $red "553-732"
+sleep 1
+echo $red "729-427"
+sleep 1
+echo $red "518-628"
+sleep 1
+echo $red "289-727"
+sleep 1
+echo $red "753-727"
+sleep 1
+echo $red "281-928"
+sleep 1
+echo $red "896-743"
+sleep 1
+echo $red "168-294"
+sleep 1
+echo $red "286-025"
+sleep 1
+echo $red "299-416"
+sleep 1
+echo $red "729-427"
+sleep 1
+echo $red "539-628"
+sleep 1
+echo $red "629-727"
+sleep 1
+echo $red "753-796"
+sleep 1
+echo $red "481-288"
+sleep 1
+echo $red "128-383"
+sleep 1
+echo $red "139-854"
+sleep 1
+echo $red "976-485"
+sleep 1
+echo $red "479-416"
+sleep 1
+echo $red "729-427"
+sleep 1
+echo $red "518-628"
+sleep 1
+echo $red "289-727"
+sleep 1
+echo $red "753-727"
+sleep 1
+echo $red "281-288"
+sleep 1
+echo $red "896-743"
+sleep 1
+echo $red "168-294"
+sleep 1
+echo $red "286-025"
+sleep 1
+echo $red "299-416"
+sleep 1
+echo $red "729-427"
+sleep 1
+echo $red "539-628"
+sleep 1
+echo $red "629-727"
+sleep 1
+echo $red "753-796"
+sleep 1
+echo $purple "Done"
+sleep 1
+echo  $blue "SUBSCRIBE : Rama Ahmed"
+
+if [ $Gans = 0 ] || [ $Gans = 00 ]
+then
+echo  $red "Bay Bay"
+echo  $blue "Jangan Lupa Coli Hari Ini!"
+echo  $green "Subscribe : Rama Ahmed" 
+exit
+fi
